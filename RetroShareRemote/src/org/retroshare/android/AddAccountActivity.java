74f1b0d1f181e7d7b4f4853a8d99979ba5e3978a
Gioacchino Mazzurco <gio@eigenlab.org>
2013-06-07 13:57:51 +0200

    added AddAccountActivity cleaned version of AddServerActivity

---
+++ RetroShareRemote/src/org/retroshare/android/AddAccountActivity.java
@@ -0,0 +1,42 @@
+package org.retroshare.android;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddAccountActivity extends RsActivityBase
+{
+	private static final String TAG="AddAccountActivity";
+	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_add_server);
+    }
+    
+    public void addServer(View v)
+    {
+    	if(mBound)
+    	{
+    		EditText editTextName     = (EditText) findViewById(R.id.editTextName);
+    		EditText editTextHostname = (EditText) findViewById(R.id.editTextHostname);
+    		EditText editTextPort     = (EditText) findViewById(R.id.editTextPort);
+    		EditText editTextUser     = (EditText) findViewById(R.id.editTextUser);
+    		
+    		RsServerData sd = new RsServerData();
+    		
+    		sd.name=editTextName.getText().toString();
+	    	sd.hostname=editTextHostname.getText().toString();
+	    	sd.port=Integer.parseInt(editTextPort.getText().toString());
+	    	sd.user=editTextUser.getText().toString();
+	    	mRsService.mRsCtrlService.setServerData(sd);
+    	}
+    	finish();
+    }
+    
+    @Override protected void onServiceConnected(){}
+
+}
