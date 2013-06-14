3e58359e32a8b0aac7f2deb9a46e5dbe8404064b
autoscatto <4utoscatto@gmail.com>
2013-06-14 13:05:00 +0200

    Clean and working repo

---
+++ RetroShareRemote/src/org/retroshare/android/ContactsSyncAdapterService.java
@@ -0,0 +1,211 @@
+package org.retroshare.android;
+
+import android.accounts.Account;
+import android.accounts.OperationCanceledException;
+import android.app.Service;
+import android.content.*;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ContactsSyncAdapterService extends Service
+{
+	private static final String TAG = "ContactsSyncAdapterService";
+	private static SyncAdapterImpl sSyncAdapter = null;
+    private static String UsernameColumn = ContactsContract.RawContacts.SYNC1;
+    private static String PhotoTimestampColumn = ContactsContract.RawContacts.SYNC2;
+    private static ContentResolver mContentResolver = null;
+
+
+	public static class SyncAdapterImpl extends AbstractThreadedSyncAdapter
+	{
+		private Context mContext;
+
+		public SyncAdapterImpl(Context context)
+		{
+			super(context, true);
+			mContext = context;
+		}
+
+		@Override
+		public void onPerformSync ( Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult )
+		{
+            //account.
+			try { ContactsSyncAdapterService.performSync(mContext, account, extras, authority, provider, syncResult); }
+			catch (OperationCanceledException e) {}
+		}
+	}
+
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		IBinder ret = null;
+		ret = getSyncAdapter().getSyncAdapterBinder();
+		return ret;
+	}
+
+	private SyncAdapterImpl getSyncAdapter()
+	{
+		if ( sSyncAdapter == null ) sSyncAdapter = new SyncAdapterImpl(this);
+		return sSyncAdapter;
+	}
+
+
+    private static void addContact(Account account, String name, String username) {
+        Log.i(TAG, "Adding contact: " + name);
+        ArrayList<ContentProviderOperation> operationList = new ArrayList<ContentProviderOperation>();
+
+        ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI);
+        builder.withValue(ContactsContract.RawContacts.ACCOUNT_NAME, account.name);
+        builder.withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, account.type);
+        builder.withValue(ContactsContract.RawContacts.SYNC1, username);
+        operationList.add(builder.build());
+
+        builder = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI);
+        builder.withValueBackReference(ContactsContract.CommonDataKinds.StructuredName.RAW_CONTACT_ID, 0);
+        builder.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+        builder.withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name);
+        operationList.add(builder.build());
+
+        builder = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI);
+        builder.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0);
+        builder.withValue(ContactsContract.Data.MIMETYPE, "retroshare.android.cursor.item/vnd.org.retroshare.android.sync.profile");
+        builder.withValue(ContactsContract.Data.DATA1, username);
+        builder.withValue(ContactsContract.Data.DATA2, "SyncProviderDemo Profile");
+        builder.withValue(ContactsContract.Data.DATA3, "View profile");
+        operationList.add(builder.build());
+
+        try {
+            mContentResolver.applyBatch(ContactsContract.AUTHORITY, operationList);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateContactStatus(ArrayList<ContentProviderOperation> operationList, long rawContactId, String status) {
+        Uri rawContactUri = ContentUris.withAppendedId(ContactsContract.RawContacts.CONTENT_URI, rawContactId);
+        Uri entityUri = Uri.withAppendedPath(rawContactUri, ContactsContract.RawContacts.Entity.CONTENT_DIRECTORY);
+        Cursor c = mContentResolver.query(entityUri, new String[] { ContactsContract.RawContacts.SOURCE_ID, ContactsContract.RawContacts.Entity.DATA_ID, ContactsContract.RawContacts.Entity.MIMETYPE, ContactsContract.RawContacts.Entity.DATA1 }, null, null, null);
+        try {
+            while (c.moveToNext()) {
+                if (!c.isNull(1)) {
+                    String mimeType = c.getString(2);
+
+                    if (mimeType.equals("retroshare.android.cursor.item/vnd.org.retroshare.android.sync.profile")) {
+                        ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(ContactsContract.StatusUpdates.CONTENT_URI);
+                        builder.withValue(ContactsContract.StatusUpdates.DATA_ID, c.getLong(1));
+                        builder.withValue(ContactsContract.StatusUpdates.STATUS, status);
+                        builder.withValue(ContactsContract.StatusUpdates.STATUS_RES_PACKAGE, "org.retroshare.android.sync");
+                        builder.withValue(ContactsContract.StatusUpdates.STATUS_LABEL, R.string.app_name);
+                        builder.withValue(ContactsContract.StatusUpdates.STATUS_ICON, R.drawable.retrosharelogo2);
+                        builder.withValue(ContactsContract.StatusUpdates.STATUS_TIMESTAMP, System.currentTimeMillis());
+                        operationList.add(builder.build());
+
+                        //Only change the text of our custom entry to the status message pre-Honeycomb, as the newer contacts app shows
+                        //statuses elsewhere
+                        if(Integer.decode(Build.VERSION.SDK) < 11) {
+                            builder = ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI);
+                            builder.withSelection(BaseColumns._ID + " = '" + c.getLong(1) + "'", null);
+                            builder.withValue(ContactsContract.Data.DATA3, status);
+                            operationList.add(builder.build());
+                        }
+                    }
+                }
+            }
+        } finally {
+            c.close();
+        }
+    }
+
+    private static void updateContactPhoto(ArrayList<ContentProviderOperation> operationList, long rawContactId, byte[] photo) {
+        ContentProviderOperation.Builder builder = ContentProviderOperation.newDelete(ContactsContract.Data.CONTENT_URI);
+        builder.withSelection(ContactsContract.Data.RAW_CONTACT_ID + " = '" + rawContactId
+                + "' AND " + ContactsContract.Data.MIMETYPE + " = '" + ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE + "'", null);
+        operationList.add(builder.build());
+
+        try {
+            if(photo != null) {
+                builder = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI);
+                builder.withValue(ContactsContract.CommonDataKinds.Photo.RAW_CONTACT_ID, rawContactId);
+                builder.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE);
+                builder.withValue(ContactsContract.CommonDataKinds.Photo.PHOTO, photo);
+                operationList.add(builder.build());
+
+                builder = ContentProviderOperation.newUpdate(ContactsContract.RawContacts.CONTENT_URI);
+                builder.withSelection(ContactsContract.RawContacts.CONTACT_ID + " = '" + rawContactId + "'", null);
+                builder.withValue(PhotoTimestampColumn, String.valueOf(System.currentTimeMillis()));
+                operationList.add(builder.build());
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    private static class SyncEntry {
+        public Long raw_id = 0L;
+        public Long photo_timestamp = null;
+    }
+
+	private static void performSync(Context context, Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) throws OperationCanceledException
+	{
+        mContentResolver = context.getContentResolver();
+
+        Log.i(TAG, "SINCAAAAAAAAAAAAAAAAAAAAAAAAAAAA------------------------------------" + account.toString());
+        Log.v(TAG, "=========================PEERSETDATA\n");
+        HashMap<String, SyncEntry> localContacts = new HashMap<String, SyncEntry>();
+        // Load the local contacts
+        Uri rawContactUri = ContactsContract.RawContacts.CONTENT_URI.buildUpon().appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_NAME, account.name).appendQueryParameter(
+                ContactsContract.RawContacts.ACCOUNT_TYPE, account.type).build();
+        Cursor c1 = mContentResolver.query(rawContactUri, new String[] { BaseColumns._ID, UsernameColumn, PhotoTimestampColumn }, null, null, null);
+        while (c1.moveToNext()) {
+            SyncEntry entry = new SyncEntry();
+            entry.raw_id = c1.getLong(c1.getColumnIndex(BaseColumns._ID));
+            entry.photo_timestamp = c1.getLong(c1.getColumnIndex(PhotoTimestampColumn));
+            localContacts.put(c1.getString(1), entry);
+        }
+
+        ArrayList<ContentProviderOperation> operationList = new ArrayList<ContentProviderOperation>();
+        String pname="canedidio";
+        Log.v(TAG, "=========================" + pname + "\n");
+        try {
+            // If we don't have any contacts, create one. Otherwise, set a
+            // status message
+            if (localContacts.get(pname) == null) {
+                addContact(account, pname+"@Location", pname);
+            } else {
+                if (localContacts.get(pname).photo_timestamp == null || System.currentTimeMillis() > (localContacts.get(pname).photo_timestamp + 604800000L)) {
+                    //You would probably download an image file and just pass the bytes, but this sample doesn't use network so we'll decode and re-compress the icon resource to get the bytes
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.retrosharelogo2);
+                    icon.compress(Bitmap.CompressFormat.PNG, 0, stream);
+                    updateContactPhoto(operationList, localContacts.get(pname).raw_id, stream.toByteArray());
+                }
+                updateContactStatus(operationList, localContacts.get(pname).raw_id, "Mille cose, Test duro");
+            }
+            if (operationList.size() > 0)
+                mContentResolver.applyBatch(ContactsContract.AUTHORITY, operationList);
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+
+
+
+
+}
