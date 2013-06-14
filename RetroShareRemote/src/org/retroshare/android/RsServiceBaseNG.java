c961921d7d3433885d530583a7d8e54712670a5c
Gioacchino Mazzurco <gio@eigenlab.org>
2013-06-14 11:09:10 +0200

    Now RetroShare peers are added to android contact list

---
+++ RetroShareRemote/src/org/retroshare/android/RsServiceBaseNG.java
@@ -0,0 +1,67 @@
+package org.retroshare.android;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * @author G10h4ck
+ * This class is aimed to be inherited by Services that needs to communicate with RsService
+ * provide out of the box almost all needed stuff to communicate with RsService
+ * so each service doesn't need to handle all this common stuff
+ */
+public abstract class RsServiceBaseNG extends Service implements ServiceConnection
+{
+	private static final String TAG="RsServiceBaseNG";
+
+	protected RsService mRsService;
+	protected boolean mBound = false;
+
+	@Override
+	public void onServiceConnected(ComponentName className, IBinder service)
+	{
+		RsService.RsBinder binder = (RsService.RsBinder) service;
+		mRsService = binder.getService();
+		mBound = true;
+		Log.v(TAG, "onServiceConnected");
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName arg0)
+	{
+		mBound = false;
+		Log.v(TAG, "onServiceDisconnected");
+	}
+
+	private void _bindRsService()
+	{
+		Intent intent = new Intent(this, RsService.class);
+		bindService(intent, this, Context.BIND_AUTO_CREATE);
+	}
+	private void _unBindRsService() { unbindService(this); }
+
+	/**
+	 * This method should be overridden by child classes that want to do something between Services.onCreate and connection initialization it is guaranteed to be executed before onServiceConnected
+	 */
+	protected void onCreateBeforeConnectionInit()
+	{}
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		onCreateBeforeConnectionInit();
+		_bindRsService();
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		_unBindRsService();
+		super.onDestroy();
+	}
+}
--- RetroShareRemote/src/org/retroshare/android/sync/ContactsSyncAdapterService.java
+++ RetroShareRemote/src/org/retroshare/android/sync/ContactsSyncAdapterService.java
@@ -2,25 +2,34 @@ package org.retroshare.android.sync;
 
 import android.accounts.Account;
 import android.accounts.OperationCanceledException;
-import android.app.Service;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SyncResult;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
 
+import org.retroshare.android.RsServiceBaseNG;
 
-public class ContactsSyncAdapterService extends Service
+import java.util.ArrayList;
+import java.util.List;
+
+import rsctrl.core.Core.Person;
+
+
+public class ContactsSyncAdapterService extends RsServiceBaseNG
 {
 	private static final String TAG = "ContactsSyncAdapterService";
 	private static SyncAdapterImpl sSyncAdapter = null;
 	private static ContentResolver mContentResolver = null;
 
-	private static class SyncAdapterImpl extends AbstractThreadedSyncAdapter
+	private class SyncAdapterImpl extends AbstractThreadedSyncAdapter
 	{
 		private Context mContext;
 
@@ -33,7 +42,7 @@ public class ContactsSyncAdapterService extends Service
 		@Override
 		public void onPerformSync ( Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult )
 		{
-			try { ContactsSyncAdapterService.performSync(mContext, account, extras, authority, provider, syncResult); }
+			try { performSync(mContext, account, extras, authority, provider, syncResult); }
 			catch (OperationCanceledException e) {}
 		}
 	}
@@ -53,10 +62,47 @@ public class ContactsSyncAdapterService extends Service
 		return sSyncAdapter;
 	}
 
-	private static void performSync(Context context, Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) throws OperationCanceledException
+	private void performSync(Context context, Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) throws OperationCanceledException
 	{
-		mContentResolver = context.getContentResolver();
 		Log.i(TAG, "performSync: " + account.toString());
+
 		//This is where the magic will happen!
+
+		if(mBound)
+		{
+			mContentResolver = context.getContentResolver();
+
+			List<Person> peers = mRsService.mRsCtrlService.peersService.getPeersList();
+			for (Person peer:peers)
+				_addContact(account, peer);
+		}
+	}
+
+	private static void _addContact(Account account, Person peer)
+	{
+		ArrayList<ContentProviderOperation> operationList = new ArrayList<ContentProviderOperation>();
+
+		String name = peer.getName();
+
+		//Create RawContact
+		ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(RawContacts.CONTENT_URI);
+		builder.withValue(RawContacts.ACCOUNT_NAME, account.name);
+		builder.withValue(RawContacts.ACCOUNT_TYPE, account.type);
+		builder.withValue(RawContacts.SYNC1, name);
+		operationList.add(builder.build());
+
+		//Create a Data record of common type 'StructuredName' for our RawContact
+		builder = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI);
+		builder.withValueBackReference(ContactsContract.CommonDataKinds.StructuredName.RAW_CONTACT_ID, 0);
+		builder.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+		builder.withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name);
+		operationList.add(builder.build());
+
+		try { mContentResolver.applyBatch(ContactsContract.AUTHORITY, operationList); }
+		catch (Exception e)
+		{
+			Log.e(TAG, "Something went wrong during creation! " + e);
+			e.printStackTrace();
+		}
 	}
 }
