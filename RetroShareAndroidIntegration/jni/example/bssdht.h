8462b670724b177750ec11876713fa5025e59e97
Gioacchino Mazzurco <gio@eigenlab.org>
2013-08-25 23:07:14 +0200

    removed bitdht experimental stuff

--- RetroShareAndroidIntegration/jni/example/bssdht.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BSSDHT_H_
-#define _BSSDHT_H_
-#include <string>
-int dowork(std::string);
-
-
-#endif //_BSSDHT_H_
--- RetroShareAndroidIntegration/src/org/retroshare/android/bitdht.java
+++
@@ -1,9 +0,0 @@
-package org.retroshare.android;
-
-public class bitdht {
-	public native static String getIp(String nodeId);
-	
-	   static {
-	       System.loadLibrary("bitdht");
-	   }
-}
