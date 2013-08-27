3a5e686ea23fb9134e6e7637eebdca13217bbba5
Gioacchino Mazzurco <gio@eigenlab.org>
2013-08-27 17:38:32 +0200

    Added License

--- RetroShareAndroidIntegration/src/org/retroshare/android/authenticator/AccountPreferenceActivity.java
+++ RetroShareAndroidIntegration/src/org/retroshare/android/authenticator/AccountPreferenceActivity.java
@@ -23,7 +23,6 @@ package org.retroshare.android.authenticator;
 import android.preference.PreferenceActivity;
 
 /**
- * @author G10h4ck
  * This empty activity is needed by Android Account Manager tho show RetroShare accounts and to edit option such as to enable/disable sync
  */
 public class AccountPreferenceActivity extends PreferenceActivity {}
--- RetroShareAndroidIntegration/src/org/retroshare/android/utils/Util.java
+++ RetroShareAndroidIntegration/src/org/retroshare/android/utils/Util.java
@@ -1,3 +1,23 @@
+/**
+ * @license
+ *
+ * Copyright (c) 2013 Gioacchino Mazzurco <gio@eigenlab.org>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.retroshare.android.utils;
 
 import android.content.ContentResolver;
@@ -26,8 +46,7 @@ public class Util
 	public static final String URI_REG_EXP = "([a-z]+[0-9a-z\\.\\-\\+]*:\\/\\/)(\\S)+"; // Probably too permissive, but I never saw a false positive in many tests
 
 	public static String TAG() { return "Util"; }
-	
-	// stolen from the internet
+
 	public static String byteArrayToHexString(byte[] b)
 	{
 		StringBuffer sb = new StringBuffer(b.length * 2);
@@ -55,7 +74,7 @@ public class Util
 	public static Bitmap encodeQrCode(String contents) throws WriterException
 	{
 		QRCode qrCode = new QRCode();
-		Encoder.encode(contents, ErrorCorrectionLevel.L, qrCode); // commented momentanuesly because raises ErrorCorrectionLevel cannot find symbol ( no time to understand what zxing related library is missing... )
+		Encoder.encode(contents, ErrorCorrectionLevel.L, qrCode);
 		
 		System.out.println("encoded qrCode: "+qrCode);
 
